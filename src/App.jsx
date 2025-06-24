74d030afde5002ffaf231b71f1251bf96d5fbfae
LanTran2006 <lantrancute2006@gmail.com>
2025-06-24 14:18:56 +0700

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
-    <>
-      <h1 className="text-center text-red-500 font-bold">Hi</h1>
-    </>
+    <div>
+      <Login/>
+    </div>
   );
 }
 
