03800d85ffae5ee0ed1fb9c9229574d1ab30f552
Hau Nguyen <nguyentrunghau7746@gmail.com>
2025-07-01 23:11:58 +0700

    fix: duplicate dropdown folder

--- src/modules/public-reporter/components/dropdown/CrimeTypes.jsx
+++ src/modules/public-reporter/components/dropdown/CrimeTypes.jsx
@@ -28,7 +28,7 @@ export default function DropdownForm() {
   };
 
   return (
-    <div className="ssm:p-6 ssm:max-w-md ssm:mt-10 mx-auto mt-6 w-full max-w-xs rounded-lg bg-[#EEEEEE] p-4 shadow-md md:max-w-lg md:p-8 xl:max-w-2xl xl:p-10">
+    <div className="bg-[#EEEEEE] p-4 ssm:p-6 md:p-8 xl:p-10 rounded-lg shadow-md w-full max-w-xs ssm:max-w-md md:max-w-lg xl:max-w-2xl mx-auto mt-6 ssm:mt-10">
       <div className="flex flex-col gap-4">
         <div
           className="relative"
@@ -39,30 +39,30 @@ export default function DropdownForm() {
         >
           <button
             type="button"
-            className="ssm:text-sm flex w-full max-w-[90vw] appearance-none items-center justify-between truncate rounded border border-gray-300 bg-white px-4 py-2 pr-8 text-xs whitespace-nowrap text-black focus:ring-2 focus:ring-blue-500 focus:outline-none md:text-base"
+            className="w-full max-w-[90vw] text-xs ssm:text-sm md:text-base appearance-none bg-white border border-gray-300 rounded px-4 py-2 pr-8 text-black focus:outline-none focus:ring-2 focus:ring-blue-500 flex justify-between items-center truncate whitespace-nowrap"
             onMouseDown={(e) => {
               e.preventDefault();
               handleFocus();
             }}
           >
             <span
-              className={selectedOption ? "truncate" : "truncate text-gray-400"}
+              className={selectedOption ? "truncate" : "text-gray-400 truncate"}
             >
               {selectedOption || "Select an option"}
             </span>
-            <span className="ml-2 text-sm text-gray-600">
+            <span className="ml-2 text-gray-600 text-sm">
               {isOpen ? "▲" : "▼"}
             </span>
           </button>
           {isOpen && (
             <ul
-              className="ssm:text-sm absolute left-0 z-10 mt-1 max-h-56 w-full max-w-[90vw] overflow-auto rounded border border-gray-300 bg-white text-xs shadow-lg md:text-base"
+              className="absolute left-0 z-10 mt-1 w-full max-w-[90vw] bg-white border border-gray-300 rounded shadow-lg max-h-56 overflow-auto text-xs ssm:text-sm md:text-base"
               style={{ minWidth: "100%" }}
             >
               {crimeTypes.map((option) => (
                 <li
                   key={option}
-                  className={`cursor-pointer px-4 py-2 hover:bg-blue-100 ${
+                  className={`px-4 py-2 cursor-pointer hover:bg-blue-100 ${
                     selectedOption === option ? "bg-blue-100" : ""
                   } truncate`}
                   onMouseDown={() => handleChange(option)}
