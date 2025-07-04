dc2e3d0d7a6428fae1af313679bc8b1ee99258b4
MockProject_062025_Nhom10 <mockprojectnhom10@gmail.com>
2025-07-04 08:32:45 +0700

    Revert "Fix : layout public reporter Nguyen quoc an"

--- src/modules/review-police/routes/ReviewPoliceRoutes.jsx
+++ src/modules/review-police/routes/ReviewPoliceRoutes.jsx
@@ -1,15 +1,7 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import CrimeReportList from "@review-police/pages/CrimeReportList";
-import CrimeReportDetail from "@review-police/pages/CrimeReportDetail";
 
 const ReviewPoliceRoutes = () => {
-  return (
-    <Routes>
-      <Route index element={<CrimeReportList />} />
-      <Route path="report-detail/:reportId" element={<CrimeReportDetail />} />
-    </Routes>
-  );
+  return <div>ReviewPoliceRoutes</div>;
 };
 
 export default ReviewPoliceRoutes;
