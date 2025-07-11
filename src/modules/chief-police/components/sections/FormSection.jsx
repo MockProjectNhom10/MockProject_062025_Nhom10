3fb2b1e073e65958bc287ba9fa037192982e9661
Quoc An <quocan@gmail.com>
2025-07-11 09:45:15 +0700

    fix: UI css for chiefpolice scr_028

--- src/modules/chief-police/components/sections/FormSection.jsx
+++ src/modules/chief-police/components/sections/FormSection.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const FormSection = ({ title, children }) => {
   return (
     <section className="mb-6 w-full rounded-xl border-1 border-gray-200 bg-white shadow-md">
-      <div className="bg-police mb-4 flex items-center justify-center rounded-t-xl py-3">
+      <div className="bg-police mb-2 flex items-center justify-center rounded-t-xl py-2">
         <h2 className="text-md font-semibold text-white">{title}</h2>
       </div>
 
-      <div className="mobile:px-1 tablet:px-2 desktop:px-5 mb-4 space-y-6">
+      <div className="mobile:px-1 tablet:px-2 desktop:px-5 mb-4 space-y-2">
         {children}
       </div>
     </section>
