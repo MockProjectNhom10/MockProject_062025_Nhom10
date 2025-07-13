86a6c7bf71c0cf10c5568549b27bbaa65d870874
Hau Nguyen <nguyentrunghau7746@gmail.com>
2025-07-13 15:35:28 +0700

    feat: using react-hook-form catch errors step1 modules reporter

--- src/modules/public-reporter/constants/index.js
+++ src/modules/public-reporter/constants/index.js
@@ -2,6 +2,9 @@ export const MESSAGES = {
   REPORT_SUCCESS: "Your report has been successfully submitted.",
 
   SUCCESS: "Navigating to Step 1",
+  // SC_002
+  INVALID_EMAIL: "Please enter a valid email in the format example@domain.com",
+  INVALID_PHONE: "Phone number must contain only digits and be 9 to 11 digits long",
 
   // SC_003
   REQUIRED: "This field is required",
