56fbcfe1133c052bee5a346cc6114599ada8d29a
Quoc An <quocan@gmail.com>
2025-07-13 13:36:41 +0700

    feat route phase 2 step2

--- src/modules/chief-police/pages/SC_017_SceneInformation.jsx
+++ src/modules/chief-police/pages/SC_017_SceneInformation.jsx
@@ -2,20 +2,12 @@ import React from "react";
 import FormCard from "@chief-police/components/sections/FormCard";
 import FormSection from "@chief-police/components/sections/FormSection";
 import Button from "@chief-police/components/common/button/Button";
-import {
-  PlusCircle,
-  CircleChevronRight,
-  Edit,
-  Trash2,
-  MonitorUp,
-} from "lucide-react";
+import { PlusCircle } from "lucide-react";
 import GenericTable from "@chief-police/components/table/GenericTable";
 
 import {
   initialStatementsColumns,
   initialStatementsData,
-  sceneDescriptionColumns,
-  sceneDescriptionData,
   mediaColumns,
   mediaData,
   evidenceColumns,
@@ -52,7 +44,11 @@ function SceneInformation() {
     >
       <FormCard
         title="INITIAL STATEMENTS"
-        button={<Button>View Details</Button>}
+        button={
+          <Button onClick={() => navigate("view-initial-statement")}>
+            View Details
+          </Button>
+        }
       >
         <GenericTable
           columns={initialStatementsColumns}
@@ -61,23 +57,12 @@ function SceneInformation() {
       </FormCard>
 
       <FormCard
-        title="SCENE DESCRIPTION"
-        button={
-          <Button classNameChildren="flex gap-2">
-            <PlusCircle size={16} /> ADD
-          </Button>
-        }
-      >
-        <GenericTable
-          columns={sceneDescriptionColumns}
-          data={sceneDescriptionData}
-        />
-      </FormCard>
-
-      <FormCard
         title="IMAGES AND VIDEO"
         button={
-          <Button classNameChildren="flex gap-2">
+          <Button
+            onClick={() => navigate("images-and-videos")}
+            classNameChildren="flex gap-2"
+          >
             <PlusCircle size={16} /> ADD
           </Button>
         }
@@ -88,27 +73,16 @@ function SceneInformation() {
       <FormCard
         title="PRELIMINARY PHYSICAL EVIDENCE INFORMATION"
         button={
-          <Button classNameChildren="flex gap-2">
+          <Button
+            onClick={() => navigate("preliminary-evidence")}
+            classNameChildren="flex gap-2"
+          >
             <PlusCircle size={16} /> ADD
           </Button>
         }
       >
         <GenericTable columns={evidenceColumns} data={evidenceData} />
       </FormCard>
-
-      <FormCard
-        title="SCENE SKETCH"
-        button={
-          <div className="flex items-center justify-between space-x-2">
-            <Trash2 className="h-4 w-4 text-red-500" />
-            <Edit className="h-4 w-4 text-blue-500" />
-            <CircleChevronRight className="h-4 w-4 text-gray-600" />
-            <Button classNameChildren="flex gap-2">
-              <MonitorUp size={16} /> UP LOAD
-            </Button>
-          </div>
-        }
-      ></FormCard>
     </FormSection>
   );
 }
